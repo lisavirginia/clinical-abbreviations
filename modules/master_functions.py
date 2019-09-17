3bc378da05a902a1dac9ed632569a104130a9fe0
Lisa V Grossman <lisa.v.grossman@gmail.com>
2019-09-17 14:49:39 -0400

    new functions

--- modules/master_functions.py
+++ modules/master_functions.py
@@ -3,7 +3,6 @@ master_functions.py
 Library of commonly used functions for the clinical abbreviation expander
 '''
 
-import pandas as pd
 import string
 
 # Function to normalize short form abbreviations
@@ -13,5 +12,5 @@ def normalized_short_form(sf):
     # Removes leading and trailing whitespace
     sf = sf.strip()
     # Removes punctuation
-    sf = sf.translate(str.maketrans('', '', string.punctuation))
+    sf = sf.translate(maketrans('', '', string.punctuation))
     return sf
