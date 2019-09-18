9abede27c7f2dd9f18450431ab0e5aae92ace63a
Lisa V Grossman <lisa.v.grossman@gmail.com>
2019-09-18 14:52:45 -0400

    new sources and functions

--- modules/master_functions.py
+++ modules/master_functions.py
@@ -3,15 +3,33 @@ master_functions.py
 Library of low-level functions
 '''
 
-import pandas as pd
-import string
+# Function to clean data frame
+def clean(df):
+  # Remove leading and trailing white space
+  cols = df.select_dtypes(['object']).columns
+  df[cols] = df[cols].apply(lambda x: x.str.strip())
+
+# Function to unnest columns in data frame
+def expand_col(df, col, d='|'):
+  # Split and stack individual entries
+  s = df[col].str.split(d).apply(pd.Series, 1).stack()
+  # Match up with df indices
+  s.index = s.index.droplevel(-1)
+  # Name new column
+  s.name = col
+  # Delete old column
+  del df[col]
+  # Merge new column with df
+  df = df.join(s)
+  return df
 
 # Function to normalize short form abbreviations
 def normalized_short_form(sf):
-    # Converts text to uppercase
-    sf = sf.upper()
-    # Removes leading and trailing whitespace
-    sf = sf.strip()
-    # Removes punctuation
-    sf = sf.translate(str.maketrans('', '', string.punctuation))
-    return sf
+  # Converts text to uppercase
+  sf = sf.upper()
+  # Removes leading and trailing whitespace
+  sf = sf.strip()
+  # Removes punctuation
+  sf = sf.translate(str.maketrans('', '', string.punctuation))
+  return sf
+
