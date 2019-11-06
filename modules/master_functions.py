4882654b043c41293c22537b0ba9403a369f9e1c
Lisa V Grossman <lisa.v.grossman@gmail.com>
2019-11-06 11:37:46 -0500

    new steps

--- modules/master_functions.py
+++ modules/master_functions.py
@@ -1,15 +1,18 @@
 '''
 master_functions.py
-Library of low-level functions
 '''
 
 import pandas as pd
+import subprocess
+import string
+
 
 # Function to clean data frame
 def clean(df):
   # Remove leading and trailing white space
   cols = df.select_dtypes(['object']).columns
   df[cols] = df[cols].apply(lambda x: x.str.strip())
+
 
 # Function to unnest columns in data frame
 def expand_col(df, col, d='|'):
@@ -25,6 +28,7 @@ def expand_col(df, col, d='|'):
   df = df.join(s)
   return df
 
+
 # Function to standardize CUI appearance
 def standardize_cui(cui):
   # Use comma delimited CUIs
@@ -33,3 +37,33 @@ def standardize_cui(cui):
   cui = cui.replace('c', 'C')
   return cui
 
+
+# Function to define normalized short form
+def normalized_short_form(sf):
+  # Convert to lowercase
+  sf = sf.lower()
+  # Strip leading and trailing whitespace
+  sf = sf.strip()
+  # Convert all punctuation to underscore
+  sf = sf.translate(str.maketrans(string.punctuation, '_'*len(string.punctuation)))
+  return sf
+
+
+'''
+Python Wrapper for UMLS Lexical Variant Generation program
+'''
+
+
+# Function to execute command line LVG program
+def lvg(file_name, flow = '', output_file = '', restrict = False, print_no_output = False, lvg_path = ''):
+    command = [lvg_path + '/bin/lvg',
+                '-i:' + file_name,
+                '-f:' + flow]
+    if output_file != '':
+        command.append('-o:' + output_file)
+    if restrict:
+        command.append('-R:1')
+    if print_no_output:
+        command.append('-n')
+    lvg_process = subprocess.check_output(command)
+    return lvg_process
