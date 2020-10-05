1f57232bae2da4c46e04b402829e3585d6a6c75c
rhgrossman <rhgrossm@yahoo.com>
2020-10-05 15:55:10 -0500

    further comments

--- code/Step4_RemoveRedundancy/group_generation/create_group_ids_new.py
+++ code/Step4_RemoveRedundancy/group_generation/create_group_ids_new.py
@@ -5,8 +5,8 @@ from tqdm import tqdm
 
 MATCH_PATH = "/ssd-1/clinical/clinical-abbreviations/data/full_prediction_check.csv"
 RECORD_PATH = "/ssd-1/clinical/clinical-abbreviations//code/Step3Output.csv"
-THRESHOLD = .78
-eliminate_list = ['ribo', 'non', 'gene', 'acid']
+MATCH_THRESHOLD = .78
+eliminate_list = ['ribo', 'non', 'gene', 'acid'] # Terms that result in suspicious matches; to be manually unmatched
 
 def _remove_suspicious_matches(df_row):
     for item in eliminate_list:
@@ -26,7 +26,7 @@ full_df.sort_values(by=['LF1'], axis=0, ascending=True)
 full_df = full_df.reset_index(inplace=False, drop=True)
 
 
-# Match already matched LFEUI entries
+# Match entries by LFEUI in 'group' column
 lfeui_match_df = record_df.dropna(axis=0, how='any', subset= ['LFEUI'], inplace=False)
 current_groups = lfeui_match_df[['SF', 'LFEUI']].groupby(['SF', 'LFEUI'], axis=0)['SF'].size().reset_index(name='Size')
 current_groups = current_groups[current_groups["Size"] > 1]
@@ -35,7 +35,7 @@ current_groups['group'] = range(cur_group_id)
 merged_record_df = record_df.merge(current_groups[['SF', 'LFEUI', 'group']], how='left', on=['SF', 'LFEUI'])
 merged_record_df['group'].fillna(0, inplace=True)
 
-#Match identical long forms
+# Match identical long forms in 'group2' column
 lf_match_df = merged_record_df[merged_record_df['LFEUI'].isnull()]
 current_groups = lf_match_df[['SF', 'LF', 'group']].groupby(['SF', 'LF'], axis=0)['LF'].size().reset_index(name='Size')
 current_groups = current_groups[current_groups["Size"] > 1]
@@ -45,10 +45,12 @@ cur_group_id = cur_group_id + len(current_groups)
 grouped_lf_match_df = lf_match_df.merge(current_groups[['SF', 'LF', 'group2']], how='left', on=['SF', 'LF'])
 merged_record_df_2 = merged_record_df.merge(grouped_lf_match_df[['RecordID', 'group2']], how='left', on=['RecordID'])
 merged_record_df_2['group2'].fillna(0, inplace=True)
+
+# Merge LFEUI and long form matches together (these sets are nonintersecting, so addition is fine)
 merged_record_df_2['group'] = merged_record_df_2['group'] + merged_record_df_2['group2']
 
-
-match_df = full_df[full_df['match_score'] > .78]
+# Only keep matches above the decided threshold
+match_df = full_df[full_df['match_score'] > MATCH_THRESHOLD]
 match_df = match_df.reset_index(inplace=False, drop=True)
 match_df['match_score'] = match_df.apply(lambda x: _remove_suspicious_matches(x), axis=1)
 
@@ -58,14 +60,18 @@ group_ids.set_index('RecordID', inplace=True)
 
 group_equivalencies = []
 for inx, row in full_df.iterrows():
-    if row['match_score'] > THRESHOLD:
+
+    if row['match_score'] > MATCH_THRESHOLD:
         id_1 = row["RecordID1"]
         id_2 = row["RecordID2"]
+        # If neither has an assigned group, assign both to a group
         if group_ids.loc[id_1, 'group'] == 0 and group_ids.loc[id_2, 'group'] == 0:
             cur_group = cur_group_id
             group_ids.loc[id_1, 'group'] = cur_group
             group_ids.loc[id_2, 'group'] = cur_group
             cur_group_id += 1
+
+        # If one has an assigned group, assign the other to that group
         elif group_ids.loc[id_1, 'group'] == 0 and group_ids.loc[id_2, 'group'] != 0:
             cur_group = group_ids.loc[id_2, 'group']
             group_ids.loc[id_1, 'group'] = cur_group
@@ -74,10 +80,13 @@ for inx, row in full_df.iterrows():
             cur_group = group_ids.loc[id_1, 'group']
             group_ids.loc[id_2, 'group'] = cur_group
 
+        # Else add the two groups to a set of group equivalencies to fix after the fact
         else:
             if group_ids.loc[id_1, 'group'] != group_ids.loc[id_2, 'group']:
                 group_equivalencies.append([group_ids.loc[id_1, 'group'], group_ids.loc[id_2, 'group']])
 
+
+# Fix group equivalencies, per the above
 group_equivalencies_set = [(min(sample), max(sample)) for sample in group_equivalencies]
 group_equivalencies_set = set(group_equivalencies_set)
 group_equivalencies_set = sorted(group_equivalencies_set, key=lambda x: x[1])
@@ -86,29 +95,36 @@ equivalencies_dict = dict(group_equivalencies_set)
 for i in range(5):
     group_ids['group'].replace(equivalencies_dict, inplace=True)
 
+# Write the group ids
 group_ids.reset_index(inplace=True, drop=False)
-
 grouped_df = record_df.merge(group_ids, how='left', on="RecordID")
 grouped_df.to_csv("/ssd-1/clinical/clinical-abbreviations/data/Step3Output_with_group.csv", index=False)
 
-fail_count = 0
-record_fails = []
-for ix, row in match_df.iterrows():
-    id_1 = row["RecordID1"]
-    id_2 = row["RecordID2"]
-    a = grouped_df[grouped_df['RecordID']==id_1][['group']].iloc[0, 0]
-    b = grouped_df[grouped_df['RecordID']==id_2][['group']].iloc[0, 0]
-    if a != b:
-        fail_count += 1
-        record_fails.append([(id_1, id_2)])
-        print('failure')
 
-for pair in record_fails:
-    id_1, id_2 = pair[0]
-    a = grouped_df[grouped_df['RecordID']==id_1][['group', 'SF','LF','LFEUI']]
-    b = grouped_df[grouped_df['RecordID']==id_2][['group', 'SF', 'LF', 'LFEUI']]
-    print(a, b)
+# Do some testing for possible mistakes, not  necesary to recieve the group ID
+def check_for_failure(grouped_df):
+    fail_count = 0
+    record_fails = []
+    for ix, row in match_df.iterrows():
+        id_1 = row["RecordID1"]
+        id_2 = row["RecordID2"]
+        a = grouped_df[grouped_df['RecordID'] == id_1][['group']].iloc[0, 0]
+        b = grouped_df[grouped_df['RecordID'] == id_2][['group']].iloc[0, 0]
+        if a != b:
+            fail_count += 1
+            record_fails.append([(id_1, id_2)])
+            print('Failure')
 
-b=grouped_df.groupby(['SF', 'LFEUI'])['group'].nunique().to_frame('size')
-b.max()
-b=grouped_df.groupby(['group'])['SF'].nunique().to_frame('size')
+    for pair in record_fails:
+        id_1, id_2 = pair[0]
+        a = grouped_df[grouped_df['RecordID']==id_1][['group', 'SF','LF','LFEUI']]
+        b = grouped_df[grouped_df['RecordID']==id_2][['group', 'SF', 'LF', 'LFEUI']]
+        print(a, b)
+
+    b = grouped_df.groupby(['SF', 'LFEUI'])['group'].nunique().to_frame('size')
+    print(b.max())
+    b = grouped_df.groupby(['group'])['SF'].nunique().to_frame('size')
+    print(b)
+    return 1
+
+check_for_failure(grouped_df)
